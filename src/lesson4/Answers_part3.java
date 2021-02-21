7f1507e7c8689e1b54490dd2bc4dd0d3d0f6d86e
Владислав <vlad@vtree.ru>
2021-02-21 19:32:03 +0800

    Урок 4. Циклы. Практика урока. Тестовые задания.

---
+++ src/lesson4/Answers_part3.java
@@ -0,0 +1,21 @@
+package lesson4;
+
+public class Answers_part3 {
+    static long factorial(long n) {
+        long fValue = 1;
+        if (n > 0) {
+            int i = 1;
+            while ( i <= n) {
+                fValue = fValue * i;
+                i++;
+            }
+        }
+        return fValue;
+    }
+
+    public static void main(String[] args) {
+        factorial(0);
+        factorial(1);
+        factorial(5);
+    }
+}
