0d0236bacd898f25fb48ae57b5cf6fb3a8a4a3b0
Vlad Sergeychuk <vlad@lionhearth.com>
2021-03-07 11:03:36 +0800

    Домашнее задание к уроку 6. Работа над ошибками. Рекомендации Наставника: "Тут у Вас тоже сделано не по тому алгоритму, который описан."
    Сделал по алгоритму задания.
    Убрал отладочные сообщения.

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -1,29 +1,27 @@
 package ru.progwards.java1.lessons.arrays;
 
-import java.util.Arrays;
-
 public class ArraySort {
 
     public static void sort(int[] a) {
-        int maxValue;
-        System.out.println("Дано:      " + Arrays.toString(a));
-        for (int i = 1; i < a.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                if (a[j] > a[i]) {
-                    maxValue = a[j];
-                    a[j] = a[i];
-                    a[i] = maxValue;
+        int buffer;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    buffer = a[i];
+                    a[i] = a[j];
+                    a[j] = buffer;
                 }
             }
         }
-        System.out.println("Результат: " + Arrays.toString(a));
     }
 
     static void myTest() {
         int[] intArray = {-17, -2205, 200, 0, 310, 1055, 90, 80, 0};
-        sort(intArray);
         intArray[4] = -900;
         intArray[5] = 15;
+        sort(intArray);
+        intArray[0] = 100;
+        intArray[7] = -500;
         sort(intArray);
     }
 
