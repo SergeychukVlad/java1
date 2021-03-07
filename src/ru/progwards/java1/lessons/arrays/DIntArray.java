06cd3a0366010ce9a1f071e25283cb610829a3f8
Vlad Sergeychuk <vlad@lionhearth.com>
2021-03-08 00:39:12 +0800

    Домашнее задание к уроку 6. Работа над ошибками. Аккуратно убрал сообщения.

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -8,20 +8,20 @@ public class DIntArray {
     public void add(int num) {
         int[] bufferArray = new int[intArray.length + 1];
         System.arraycopy(intArray, 0, bufferArray, 0, intArray.length);
-        System.out.println("add()");
-        System.out.println("Дано:      " + Arrays.toString(intArray));
-        System.out.println("Добавляем элемент со значением " + num + " в конец массива");
+//        System.out.println("add()");
+//        System.out.println("Дано:      " + Arrays.toString(intArray));
+//        System.out.println("Добавляем элемент со значением " + num + " в конец массива");
         bufferArray[intArray.length] = num;
         intArray = bufferArray;
-        System.out.println("Результат: " + Arrays.toString(intArray));
-        System.out.println();
+//        System.out.println("Результат: " + Arrays.toString(intArray));
+//        System.out.println();
     }
 
     public void atInsert(int pos, int num) {
         int[] bufferArray = new int[intArray.length + 1];
-        System.out.println("atInsert()");
-        System.out.println("Дано:      " + Arrays.toString(intArray));
-        System.out.println("Добавляем в позицию " + pos + " элемент со значением " + num);
+//        System.out.println("atInsert()");
+//        System.out.println("Дано:      " + Arrays.toString(intArray));
+//        System.out.println("Добавляем в позицию " + pos + " элемент со значением " + num);
         System.arraycopy(intArray, 0, bufferArray, 0, pos);
         for (int i = 0; i < pos; i++) {
             bufferArray[i] = intArray[i];
@@ -31,15 +31,15 @@ public class DIntArray {
             bufferArray[i + 1] = intArray[i];
         }
         intArray = bufferArray;
-        System.out.println("Результат: " + Arrays.toString(intArray));
-        System.out.println();
+//        System.out.println("Результат: " + Arrays.toString(intArray));
+//        System.out.println();
     }
 
     public void atDelete(int pos) {
         int[] bufferArray = new int[intArray.length - 1];
-        System.out.println("atDelete()");
-        System.out.println("Дано:      " + Arrays.toString(intArray));
-        System.out.println("Удаляем элемент с индексом " + pos);
+//        System.out.println("atDelete()");
+//        System.out.println("Дано:      " + Arrays.toString(intArray));
+//        System.out.println("Удаляем элемент с индексом " + pos);
         for (int i = 0; i < pos; i++) {
             bufferArray[i] = intArray[i];
         }
@@ -47,18 +47,18 @@ public class DIntArray {
             bufferArray[i] = intArray[i + 1];
         }
         intArray = bufferArray;
-        System.out.println("Результат: " + Arrays.toString(intArray));
-        System.out.println();
+//        System.out.println("Результат: " + Arrays.toString(intArray));
+//        System.out.println();
     }
 
     public int at(int pos) {
         int result;
-        System.out.println("at()");
-        System.out.println("Дано:      " + Arrays.toString(intArray));
-        System.out.println("Возвращаем элемент с индексом " + pos);
-        System.out.println("На позиции " + pos + " находится элемент со значением " + intArray[pos]);
+//        System.out.println("at()");
+//        System.out.println("Дано:      " + Arrays.toString(intArray));
+//        System.out.println("Возвращаем элемент с индексом " + pos);
+//        System.out.println("На позиции " + pos + " находится элемент со значением " + intArray[pos]);
         result = intArray[pos];
-        System.out.println();
+//        System.out.println();
         return result;
     }
 
