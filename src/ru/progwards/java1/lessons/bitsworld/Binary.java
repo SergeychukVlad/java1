369ecb71040a708179f018a9212036b08d6c4d55
Владислав <vlad@vtree.ru>
2021-03-13 21:51:09 +0800

    Урок 7. Домашнее задание. Подготовка.

---
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -0,0 +1,26 @@
+package ru.progwards.java1.lessons.bitsworld;
+
+public class Binary {
+    /*
+
+Задача 3. Класс Binary
+3.1 Реализовать конструктор public Binary(byte num).
+3.2 Реализовать метод public String toString(), который возвращает двоичное представление числа типа byte,
+используя только битовые операции. В выводимом значении всегда должно быть 8 символов
+Например:
+0:      "00000000"
+1:      "00000001"
+127:    "01111111"
+-128:   "10000000"
+-1:     "11111111"
+
+     */
+
+    static void myTest() {
+
+    }
+
+    public static void main(String[] args) {
+        myTest();
+    }
+}
---
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -0,0 +1,23 @@
+package ru.progwards.java1.lessons.bitsworld;
+
+public class SumBits {
+    /*
+
+Задача 1. Класс SumBits
+1.1 Реализовать функцию public static int sumBits(byte value), которая суммирует все биты параметра value.
+Например, для двоичного числа 0100101 функция должна вернуть 3.
+
+Подсказки:
+используйте & с числом 1 для того, чтобы оставить только один правый значащий бит;
+используйте сдвиг вправо для того, чтобы проверить следующий бит.
+
+     */
+
+    static void myTest() {
+
+    }
+
+    public static void main(String[] args) {
+        myTest();
+    }
+}
