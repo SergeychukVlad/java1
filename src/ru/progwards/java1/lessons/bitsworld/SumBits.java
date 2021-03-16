8862747dbd7c85cb7480e7400de44f5fea5809e9
Владислав <vlad@vtree.ru>
2021-03-16 23:14:04 +0800

    Урок 7. Домашнее задание. Работа над ошибками. Изменил метод sumBits(), используя битовые операции.

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -11,9 +11,15 @@ public class SumBits {
 используйте & с числом 1 для того, чтобы оставить только один правый значащий бит;
 используйте сдвиг вправо для того, чтобы проверить следующий бит.
 
+-128:   "10000000"
+1:      "00000001"
+127:    "01111111"
+0:      "00000000"
+-1:     "11111111"
+
      */
 
-    public static int sumBits(byte value) {
+    public static int sumBitsOnSubstring(byte value) {
         String binaryString = Integer.toBinaryString(value);
         int result = 0;
         if (value < 0) { // для отрицательного value. Обрезать до 8-ми знаков.
@@ -30,8 +36,25 @@ public class SumBits {
         return result;
     }
 
+    public static int sumBits(byte value) {
+        int result = 0;
+        byte mask = (byte) 1;
+
+        if (value == 0) {
+            return 0;
+        } else {
+            for (int i = 0; i < 8; i++) {
+                if ((value & mask) != 0) {
+                    result++;
+                }
+                mask <<= 1;
+            }
+        }
+        return result;
+    }
+
     static void myTest() {
-        byte[] testArray = {0, 1, 127, -128, -1};
+        byte[] testArray = {-128, 1, 127, 0, -1};
         for (int i = 0; i < testArray.length; i++) {
             System.out.println(sumBits(testArray[i]));
         }
