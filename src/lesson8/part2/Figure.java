963303a9dec23cfb00d6c0d3cd21cfff37bf14ba
Владислав <vlad@vtree.ru>
2021-03-18 12:41:40 +0800

    Урок 8. Часть 2. Абстрактные классы. Практика урока.

---
+++ src/lesson8/part2/Figure.java
@@ -0,0 +1,11 @@
+package lesson8.part2;
+
+public abstract class Figure {
+    //    perimeter
+    abstract double perimeter();
+
+    //area
+    double area() {
+        return 0d;
+    }
+}
---
+++ src/lesson8/part2/Figure3D.java
@@ -0,0 +1,5 @@
+package lesson8.part2;
+
+public abstract class Figure3D extends Figure {
+    abstract public double volume();
+}
