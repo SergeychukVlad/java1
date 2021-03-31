fe00810ae962816d74277506bb7bc59f201d6c8c
Владислав <vlad@vtree.ru>
2021-03-31 15:11:46 +0800

    Урок 8. ДЗ. Задача 3. Вариант 1.

---
+++ src/ru/progwards/java1/lessons/interfaces/ArraySort.java
@@ -0,0 +1,43 @@
+package ru.progwards.java1.lessons.interfaces;
+
+public class ArraySort implements CompareWeight {
+    public int value;
+
+    public ArraySort(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public CompareResult compareWeight(CompareWeight smthValue) {
+        if (this.value < ((ArraySort) smthValue).getValue()) {
+            return CompareResult.LESS;
+        }
+        if (this.value > ((ArraySort) smthValue).getValue()) {
+            return CompareResult.GREATER;
+        } else
+            return CompareResult.EQUAL;
+    }
+
+    public static void sort(CompareWeight[] a) {
+        CompareWeight buffer;
+        for (int i = 1; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (a[j].compareWeight(a[i]) == CompareWeight.CompareResult.GREATER) {
+                    buffer = a[j];
+                    a[j] = a[i];
+                    a[i] = buffer;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArraySort[] values = {new ArraySort(500), new ArraySort(169), new ArraySort(1024),
+                new ArraySort(100), new ArraySort(7)};
+        sort(values);
+    }
+}
---
+++ src/ru/progwards/java1/lessons/interfaces/CompareWeight.java
@@ -0,0 +1,11 @@
+package ru.progwards.java1.lessons.interfaces;
+
+public interface CompareWeight {
+    enum CompareResult {
+        LESS,
+        EQUAL,
+        GREATER
+    }
+
+    CompareResult compareWeight(CompareWeight smthHasWeight);
+}
