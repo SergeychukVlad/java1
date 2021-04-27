81ab1e31d712f20c29fb782e53d095a2e794c2fd
Владислав <vlad@vtree.ru>
2021-04-27 10:53:46 +0800

    Урок 9. Домашнее задание. Вариант 2. Вернул классический swith-case

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -30,6 +30,39 @@ public class ArrayInteger {
         for (int i = 0; i < rangeOfArray; i++) {
             digits[i] = bytes[i];
             switch (bytes[i]) {
+                case 48:
+                    digits[i] = 0;
+                    break;
+                case 49:
+                    digits[i] = 1;
+                    break;
+                case 50:
+                    digits[i] = 2;
+                    break;
+                case 51:
+                    digits[i] = 3;
+                    break;
+                case 52:
+                    digits[i] = 4;
+                    break;
+                case 53:
+                    digits[i] = 5;
+                    break;
+                case 54:
+                    digits[i] = 6;
+                    break;
+                case 55:
+                    digits[i] = 7;
+                    break;
+                case 56:
+                    digits[i] = 8;
+                    break;
+                case 57:
+                    digits[i] = 9;
+                    break;
+            }
+/*  вариант enhanced switch от Idea
+            switch (bytes[i]) {
                 case 48 -> digits[i] = 0;
                 case 49 -> digits[i] = 1;
                 case 50 -> digits[i] = 2;
@@ -41,6 +74,8 @@ public class ArrayInteger {
                 case 56 -> digits[i] = 8;
                 case 57 -> digits[i] = 9;
             }
+ */
+
         }
     }
 
