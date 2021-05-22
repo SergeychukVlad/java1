511a544a7341719fc817b324d07f498297b23b72
Владислав <vlad@vtree.ru>
2021-05-22 08:13:21 +0800

    Урок 10. Домашнее задание. Вариант 14. Сохранил лог с ошибкой в имени файла.

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -12,8 +12,8 @@ import java.io.*;
 public class Coder {
     private static final String inFileName = "source.txt";          // исходный файл (цифры)- читаем ДЛЯ кодирования
     private static final String outFileName = "coded.txt";          // файл для хранения (буквы) - пишем ЗАКОДИРОВАННОЕ
-    private static final String decodedFileName = "decoded/.txt";    // файл ДЛЯ сохранения РАСКОДИРОВАННОГО (ждём цифры)
-    private static final String logName = "coder.log";              // файл для хранения логов
+    private static final String decodedFileName = "decoded.txt";    // файл ДЛЯ сохранения РАСКОДИРОВАННОГО (ждём цифры)
+    private static final String logName = "log.txt";                // файл для хранения логов
     private static char[] code = new char[256];                     // массив символов (ключ - символ)
     private static int codeShift = 25;                              // ключ - смещение
 
