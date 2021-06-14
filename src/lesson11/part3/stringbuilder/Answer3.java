2ed9803369cf0a04fab8cbf8c12463f74cebe1d6
Владислав <vlad@vtree.ru>
2021-06-15 00:35:21 +0800

    Урок 11. Часть 3. Раздел 2. Строковые операции. Тестовые задания. Добавил invertWords2().

--- src/lesson11/part3/stringbuilder/Answer3.java
+++ src/lesson11/part3/stringbuilder/Answer3.java
@@ -1,9 +1,11 @@
 package lesson11.part3.stringbuilder;
 
+import java.util.Arrays;
+
 public class Answer3 {
-    static String invertWords(String sentence) {
-        String[] sentenceArr = sentence.split(" ");
+    static String invertWords1(String sentence) {
         StringBuilder stringBuilder = new StringBuilder();
+        String[] sentenceArr = sentence.split(" ");
         for (int i = sentenceArr.length - 1; i >= 0; i--) {
             if (i == 0) {
                 stringBuilder.append(sentenceArr[i]);
@@ -13,7 +15,20 @@ public class Answer3 {
         return stringBuilder.toString();
     }
 
+    static String invertWords2(String sentence) {
+        String reverseSentence = "";
+        String[] sentenceArr = sentence.split(" ");
+        for (int i = 0; i < sentenceArr.length; i++) {
+            if (i == sentenceArr.length - 1) {
+                reverseSentence += sentenceArr[(sentenceArr.length - 1) - i];
+            } else
+                reverseSentence += sentenceArr[(sentenceArr.length - 1) - i] + ".";
+        }
+        return reverseSentence;
+    }
+
     public static void main(String[] args) {
-        System.out.println(invertWords("Буря мглою небо кроет"));
+        System.out.println(invertWords1("Буря мглою небо кроет"));
+        System.out.println(invertWords2("Буря мглою небо кроет"));
     }
 }
