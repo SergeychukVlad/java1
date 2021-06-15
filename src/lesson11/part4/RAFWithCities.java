7961f5edef68c52fb126fa1a0533487eb4d14a01
Владислав <vlad@vtree.ru>
2021-06-15 20:29:59 +0800

    Урок 11. Часть 4. Random Access File. Упражнения.

--- src/lesson11/part4/RAFWithCities.java
+++ src/lesson11/part4/RAFWithCities.java
@@ -5,7 +5,29 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class RAFWithCities extends RandomAccessFile {
-    public static class CityGeoLocation{}
+    public static class CityGeoLocation {
+        public String city;
+        public double lat;
+        public double lon;
+
+        public CityGeoLocation() {
+            city = "";
+        }
+
+        // формируем значение по строке
+        public CityGeoLocation(String cityStr) {
+            if (cityStr == null) return;
+            String[] cityArr = cityStr.split(",");
+            city = cityArr[0].toUpperCase();
+            if (cityArr.length > 1) lat = Double.parseDouble(cityArr[1]);
+            if (cityArr.length > 2) lon = Double.parseDouble(cityArr[2]);
+        }
+
+        @Override
+        public String toString() {
+            return city + " (" + lat + ", " + lon + ")";
+        }
+    }
 
     // свойства
     private long start = 0;
@@ -14,11 +36,21 @@ public class RAFWithCities extends RandomAccessFile {
     public CityGeoLocation cityGeoLocation = new CityGeoLocation();     // выбранный город
 
     // методы
-    public RAFWithCities(String pathName) throws FileNotFoundException{}
+    public RAFWithCities(String pathName) throws FileNotFoundException {
+        super(pathName, "r");
+    }
+
     public boolean findCity(String city) throws IOException {
         return false;
     }
-    private long startStringPosition(long pos) throws  IOException{
+
+    private long startStringPosition(long pos) throws IOException {
+        // ищем символ конца строки 0x0D (\r) или 0x0A (\n)
+        for (long i = pos; i >= 0; i--) {
+            seek(i);
+            int byteChar = read();
+            if (byteChar == 0x0D || byteChar == 0x0A) return i + 1;
+        }
         return 0;
     }
 
