461aebd43a221c754bf1f2bd21aa048ce426532a
Владислав <vlad@vtree.ru>
2021-07-02 01:21:49 +0800

    Урок 11. Задача 3. Вариант 1.

--- src/ru/progwards/java1/lessons/io2/Censor.java
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -16,12 +16,71 @@ obscene = {"Java", "Oracle", "Sun", "Microsystems"}
  */
 package ru.progwards.java1.lessons.io2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Censor {
+
+    private static char getSymbol(char symbol) {
+        char[] punctuationSymbols = {' ', ',', '-', ':', '.', '!', '?', '\r', '\n'};
+        for (char punctuationSymbol : punctuationSymbols) {
+            if (symbol == punctuationSymbol) break;
+        }
+        return symbol;
+    }
+
+    private static String getStarsWord(String[] obscene, String[] stars, String separateWord) {
+        for (int j = 0; j < obscene.length; j++) {
+            if (separateWord.equals(obscene[j])) {
+                separateWord = stars[j];
+                break;
+            }
+        }
+        return separateWord;
+    }
+
+
     public static void censorFile(String inoutFileName, String[] obscene) {
+        // сделали массив-клон со звездочками вместо символов оригинального obscene[]
+        String[] stars = obscene.clone();
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = "*".repeat(stars[i].length());
+        }
+
+        String sentence = "";
+        StringBuilder separateWord = new StringBuilder();
+
+        File file = new File(inoutFileName);
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                sentence = scanner.nextLine();
+                System.out.println(sentence);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден!");
+        }
+
+        StringBuilder result = new StringBuilder();
+        char[] charsOfSentence = sentence.toCharArray();
+        for (int i = 0; i < charsOfSentence.length; i++) {
+            if (Character.isLetter(charsOfSentence[i])) {
+                separateWord.append(charsOfSentence[i]);
+
+                // если строка не заканчивается знаком препинания (или \r), то "ловим" слово на индексе окончания строки
+                if (i == charsOfSentence.length - 1) {
+                    result.append(getStarsWord(obscene, stars, separateWord.toString()));
+                }
+            } else {
+                result.append(getStarsWord(obscene, stars, separateWord.toString())).append(getSymbol(charsOfSentence[i]));
+                separateWord = new StringBuilder();
+            }
+        }
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
         String[] obscene = {"Java", "Class", "Library", "Synchronized", "Accessor"};
-        censorFile("hw_lesson11_task3", obscene);
+        censorFile("D:\\Progwards\\src\\ru\\progwards\\java1\\lessons\\io2\\hw_lesson11_task3", obscene);
     }
 }
