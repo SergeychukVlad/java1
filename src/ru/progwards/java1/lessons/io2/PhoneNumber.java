49839cab1c783d8e58e4da66d87fc03c37496db1
Владислав <vlad@vtree.ru>
2021-07-12 23:48:48 +0800

    Урок 11. Задача 1. В проверку. Вариант 1.

--- src/ru/progwards/java1/lessons/io2/PhoneNumber.java
+++ src/ru/progwards/java1/lessons/io2/PhoneNumber.java
@@ -15,12 +15,43 @@
  */
 package ru.progwards.java1.lessons.io2;
 
+import java.util.Scanner;
+
 public class PhoneNumber {
+    public static String getResultPhoneNumber(String phoneNumber) {
+        if (phoneNumber.charAt(0) == '7' || phoneNumber.charAt(0) == '8') {
+            phoneNumber = "+7(" + phoneNumber.substring(1, 4) + ")"
+                    + phoneNumber.substring(5, 7) + "-" + phoneNumber.substring(7);
+        }
+        return phoneNumber;
+    }
+
     public static String format(String phone) {
-        return null;
+        StringBuilder sourcePhoneNumber = new StringBuilder();
+        try (Scanner scanner = new Scanner(phone).useDelimiter("\\s*,\\s*")) {
+            while (scanner.hasNextLine()) {
+                char[] chars = scanner.next().toCharArray();
+                for (char aChar : chars) {
+                    if (Character.isDigit(aChar)) {
+                        sourcePhoneNumber.append(aChar);
+                    }
+                }
+
+                if ((sourcePhoneNumber.length() < 10) || (sourcePhoneNumber.length() > 11)) {
+                    throw new Exception();
+                } else {
+                    phone = getResultPhoneNumber(sourcePhoneNumber.toString());
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("В исходном телефонном номере неверное количество цифр: требуется 10 или 11, а имеем "
+                    + sourcePhoneNumber.length());
+        }
+        return phone;
     }
 
     public static void main(String[] args) {
+        format("7999111223345");
         format("79991112233");
         format("8(999)111-22-33");
         format("8 999 111 22 33");
