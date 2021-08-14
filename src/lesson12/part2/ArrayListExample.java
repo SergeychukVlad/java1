7d1778d2897aec724545f3381c4637462e9e9d4a
Владислав <vlad@vtree.ru>
2021-08-14 12:41:58 +0800

    Урок 12. Часть 2. ArrayList. Упражнения.

--- src/lesson12/part2/ArrayListExample.java
+++ src/lesson12/part2/ArrayListExample.java
@@ -1,35 +1,31 @@
 package lesson12.part2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ArrayListExample {
 
     public static ArrayList creation() {
-        List arrayList = new ArrayList<Integer>(10);
-        arrayList.add(17);
-        arrayList.add(22);
-        arrayList.add(75);
-        arrayList.add(175);
-        arrayList.add(275);
-        arrayList.add(375);
-        arrayList.add(1, 109);
-        System.out.println("отработал " + new Throwable().getStackTrace()[0].getMethodName() + "()\n" + arrayList);
+        System.out.println(new Throwable().getStackTrace()[0].getMethodName() + "работает... ");
+        List<Integer> arrayList = new ArrayList(Arrays.asList(17, 22, 75, 175, 275, 375));
+        System.out.println(arrayList);
         return (ArrayList) arrayList;
     }
 
     public static ArrayList action(ArrayList arrayList) {
+        System.out.println(new Throwable().getStackTrace()[0].getMethodName() + "()\n" + arrayList + "работает... ");
+        arrayList.add(1, 109);
         arrayList.remove(arrayList.size() - 1); // удалили последний элемент
-        System.out.println("отработал " + new Throwable().getStackTrace()[0].getMethodName() + "()\n" + arrayList);
         return arrayList;
     }
 
-    public static ArrayList specific(ArrayList arrayList) {
-        arrayList.ensureCapacity(4); // уменьшили размер до 4 элементов
-        System.out.println("отработал " + new Throwable().getStackTrace()[0].getMethodName() + "() and ensureCapacity\n" + arrayList);
-        arrayList.trimToSize(); // сжать массив до реального кол-ва элементов в нём
-        System.out.println("отработал " + new Throwable().getStackTrace()[0].getMethodName() + "() and trimToSize\n" + arrayList);
-        return arrayList;
+    public static void specific(ArrayList arrayList) {
+        System.out.println(new Throwable().getStackTrace()[0].getMethodName() + "работает... ");
+        arrayList.ensureCapacity(4);
+        System.out.println("уменьшили размер до 4 элементов - ensureCapacity: " + arrayList);
+        arrayList.trimToSize();
+        System.out.println("сжать массив до реального кол-ва элементов в нём - trimToSize: " + arrayList);
     }
 
     public static void main(String[] args) {
