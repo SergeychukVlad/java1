1c5027adfb62fbc7d0be20ef5a0662492bbbbc5c
Vlad Sergeychuk <vlad@lionhearth.com>
2021-08-17 13:00:38 +0800

    Урок 12. Часть 2. Упражнения. Пример использования removeIf().

--- src/lesson12/part2/ArrayListExample.java
+++ src/lesson12/part2/ArrayListExample.java
@@ -7,23 +7,29 @@ import java.util.List;
 public class ArrayListExample {
 
     public static ArrayList creation() {
-        System.out.println(new Throwable().getStackTrace()[0].getMethodName() + "работает... ");
+        System.out.println(new Throwable().getStackTrace()[0].getMethodName() + " работает... ");
         List<Integer> arrayList = new ArrayList(Arrays.asList(17, 22, 75, 175, 275, 375));
-        System.out.println(arrayList);
+        System.out.println(arrayList + "\n");
         return (ArrayList) arrayList;
     }
 
     public static ArrayList action(ArrayList arrayList) {
-        System.out.println(new Throwable().getStackTrace()[0].getMethodName() + "()\n" + arrayList + "работает... ");
+        System.out.println(new Throwable().getStackTrace()[0].getMethodName() + " работает... ");
         arrayList.add(1, 109); // добавил элемент по индексу
         arrayList.remove(arrayList.size() - 1); // удалили последний элемент
+        System.out.println(arrayList + "\n");
         return arrayList;
     }
 
     public static void specific(ArrayList arrayList) {
-        System.out.println(new Throwable().getStackTrace()[0].getMethodName() + "работает... ");
-        arrayList.ensureCapacity(4);
-        System.out.println("уменьшили размер до 4 элементов - ensureCapacity: " + arrayList);
+        System.out.println(new Throwable().getStackTrace()[0].getMethodName() + " работает... ");
+
+        arrayList.removeIf(i -> (i.equals(109) || i.equals(275))); // удалили элементы 109, 275
+        System.out.println("удалили элементы со значениями 109, 275 - removeIf: " + arrayList);
+        arrayList.ensureCapacity(14);
+        System.out.println("увеличили ёмкость резервного массива до 14 элементов - ensureCapacity: " + arrayList);
+        arrayList.addAll(Arrays.asList(null, null));
+        System.out.println("добавили пару NULL - addAll: " + arrayList);
         arrayList.trimToSize();
         System.out.println("сжать массив до реального кол-ва элементов в нём - trimToSize: " + arrayList);
     }
