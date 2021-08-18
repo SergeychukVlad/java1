f06d739005d57edfbe17e114e0108c5555b8912a
Vlad Sergeychuk <vlad@lionhearth.com>
2021-08-19 01:12:40 +0800

    Урок 12. Домашнее задание. Часть 2. Implemented findSimilar(). Нет обработки последнего элемента!

--- src/ru/progwards/java1/lessons/collections/Finder.java
+++ src/ru/progwards/java1/lessons/collections/Finder.java
@@ -51,7 +51,7 @@ public class Finder {
 
     public static boolean findSequence(Collection<Integer> numbers) {
         for (int i = 1; i < numbers.size(); i++) {
-            if (numbers.contains(i) == false) { // ищем единственное несовпадение. Если не находим - возвращаем true.
+            if (!numbers.contains(i)) { // ищем единственное несовпадение. Если не находим - возвращаем true.
                 return false;
             }
         }
@@ -59,7 +59,26 @@ public class Finder {
     }
 
     public static String findSimilar(Collection<String> names) {
-        ArrayList<String> words = new ArrayList<>(names);
+        ArrayList<String> arrayListNames = new ArrayList<>(names);
+        ArrayList<String> words = new ArrayList<>();
+        ArrayList<Integer> nTimes = new ArrayList<>();
+        System.out.println(arrayListNames);
+        String name = "";
+        int j = 1;  // количество совпадений для конкретного элемента
+
+        for (int i = 0; i < arrayListNames.size() - 1; i++) {
+            if (arrayListNames.get(i).equals(arrayListNames.get(i + 1))) {
+                j++;
+            } else {
+                name = arrayListNames.get(i);
+                words.add(name);
+                nTimes.add(j);
+                j = 1;
+            }
+
+        }
+        System.out.println(words);
+        System.out.println(nTimes);
         return null;
 
     }
@@ -67,7 +86,7 @@ public class Finder {
     public static void main(String[] args) {
         ArrayList<Integer> myList = new ArrayList<>(Arrays.asList(1, 3, -2, 10, 45, 14, -6, 55, 19));
         ArrayList<Integer> myListForTrue = new ArrayList<>(Arrays.asList(1, 2, 3));
-        ArrayList<String> myStringList = new ArrayList<>(Arrays.asList("Тихон", "Таня", "Тихон", "Тихон", "Толя", "Толя", "Толя", "Таня", "Таня"));
+        ArrayList<String> myStringList = new ArrayList<>(Arrays.asList("Тихон", "Таня", "Тихон", "Тихон", "Толя", "Толя", "Толя", "Таня", "Таня", "Алекс"));
 
         System.out.println("findMinSumPair(): " + findMinSumPair(myList));
 
