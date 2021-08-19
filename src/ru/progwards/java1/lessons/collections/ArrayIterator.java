67507f583e87d916d20d7a755a3b1917da2880d7
Vlad Sergeychuk <vlad@lionhearth.com>
2021-08-19 14:38:10 +0800

    Урок 12. Домашнее задание. Часть 3. Имплементировал Класс ArrayIterator. В проверку. Вариант 1.

--- src/ru/progwards/java1/lessons/collections/ArrayIterator.java
+++ src/ru/progwards/java1/lessons/collections/ArrayIterator.java
@@ -1,4 +1,24 @@
 package ru.progwards.java1.lessons.collections;
 
-public class ArrayIterator {
+import java.util.Iterator;
+
+public class ArrayIterator<T> implements Iterator<T> {
+
+    private T[] array;
+
+    public ArrayIterator(T[] array) {
+        this.array = array;
+    }
+
+    private int currentIndex = 0;
+
+    @Override
+    public boolean hasNext() {
+        return currentIndex < this.array.length && array[currentIndex] != null;
+    }
+
+    @Override
+    public T next() {
+        return array[currentIndex++];
+    }
 }
