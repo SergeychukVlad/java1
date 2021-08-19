4149cc3392ce4ee7875dea06915947a423d112e3
Vlad Sergeychuk <vlad@lionhearth.com>
2021-08-19 20:00:43 +0800

    Урок 12. Домашнее задание. Часть 3. MatrixIterator. Реализовал по аналогии с ArrayIterator. Вся фишка в конструкторе. В проверку.

--- src/ru/progwards/java1/lessons/collections/IteratorTest.java
+++ src/ru/progwards/java1/lessons/collections/IteratorTest.java
@@ -5,21 +5,22 @@ public class IteratorTest {
         Integer[] forArrayIterator = new Integer[]{1, 3, 67, 89, 222};
         ArrayIterator<Integer> arrayIterator = new ArrayIterator<>(forArrayIterator);
         withArrayIterator(arrayIterator);
+        System.out.println();
 
-        Integer[][] forMatrixIterator = new Integer[][]{};
+        Integer[][] forMatrixIterator = new Integer[][]{{1, 2, 3}, {40, 50, 60}, {700, 800, 900}};
         MatrixIterator<Integer> matrixIterator = new MatrixIterator<>(forMatrixIterator);
         withMatrixIterator(matrixIterator);
     }
 
     public static void withArrayIterator(ArrayIterator<Integer> arrayIterator) {
         while (arrayIterator.hasNext()) {
-            System.out.println(arrayIterator.next());
+            System.out.print(arrayIterator.next() + " ");
         }
     }
 
     public static void withMatrixIterator(MatrixIterator<Integer> matrixIterator) {
         while (matrixIterator.hasNext()) {
-            System.out.println(matrixIterator.next());
+            System.out.print(matrixIterator.next() + " ");
         }
     }
 }
--- src/ru/progwards/java1/lessons/collections/MatrixIterator.java
+++ src/ru/progwards/java1/lessons/collections/MatrixIterator.java
@@ -1,26 +1,45 @@
+/*
+Класс MatrixIterator
+Сделать итератор MatrixIterator по двумерному массиву (матрице), который разворачивает матрицу
+в линейную последовательность построчно: a[0][0], a[0][1], ...a[0][N],a[1][0], a[1][1]...a[1][N]... a[M][N]
+
+Шаблон для итератора взять от ArrayIterator
+ */
 package ru.progwards.java1.lessons.collections;
 
 import java.util.Iterator;
 
 public class MatrixIterator<T> implements Iterator<T> {
 
-    private T[][] array;
+    private T[] array;
 
-    public MatrixIterator(T[][] array) {
-        this.array = array;
+    public MatrixIterator(T[][] arrayAsParam) {
+        // создаю одномерный массив для "укладки" элементов двухмерного
+        T[] oneSizeArray = (T[]) new Object[arrayAsParam.length * arrayAsParam.length];
+        int k = 0;                          // счётчик одномерного массива
+        int size = arrayAsParam.length      // счётчик одномерного массива: кол-во строк
+                * arrayAsParam[0].length;   // * кол-во столбцов
+
+        while (k < size) {
+            for (T[] ts : arrayAsParam) {
+                for (int i = 0; i < arrayAsParam.length; i++) {
+                    oneSizeArray[k] = ts[i];
+                    k++;
+                }
+            }
+        }
+        this.array = oneSizeArray;
     }
-
+    // дальше всё, как в ArrayIterator
     private int currentIndexI = 0;
-    private int currentIndexJ = 0;
 
     @Override
     public boolean hasNext() {
-        return (currentIndexI < this.array.length && array[currentIndexI] != null)
-                && (currentIndexJ < this.array.length && array[currentIndexJ] != null);
+        return currentIndexI < this.array.length && array[currentIndexI] != null;
     }
 
     @Override
     public T next() {
-        return array[currentIndexI++][currentIndexJ++];
+        return array[currentIndexI++];
     }
 }
