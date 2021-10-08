8e5874abaadd619d701158a30b7b9192dfdc5c07
Vlad Sergeychuk <vlad@lionhearth.com>
2021-10-08 16:08:31 +0800

    Урок 13. Домашнее задание. В проверку. Вариант 1.

--- src/ru/progwards/java1/lessons/sets/SetOperations.java
+++ src/ru/progwards/java1/lessons/sets/SetOperations.java
@@ -9,5 +9,42 @@ https://studopedia.ru/14_138615_operatsii-nad-mnozhestvami-i-ih-svoystva.html и
  */
 package ru.progwards.java1.lessons.sets;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class SetOperations {
+
+    public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> result = new TreeSet<>(set1);
+        result.addAll(set2);
+        return result;
+    }
+
+    public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> result = new TreeSet<>(set1);
+        result.retainAll(set2);
+        return result;
+    }
+
+    public static Set<Integer> difference(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> result = new TreeSet<>(set1);
+        result.removeAll(intersection(set1, set2));
+        return result;
+    }
+
+    public static Set<Integer> symDifference(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> result = new TreeSet<>(union(set1, set2));
+        result.removeAll(intersection(set1, set2));
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = Set.of(1, -3, 6, 8, 0, -4, 4);
+        Set<Integer> set2 = Set.of(-9, -7, -1, 19, 7, 10, -4, 4, 0);
+
+        System.out.println(union(set1, set2));
+        System.out.println(intersection(set1, set2));
+        System.out.println(difference(set1, set2));
+        System.out.println(symDifference(set1, set2));
+    }
 }
