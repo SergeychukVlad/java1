a8b166b920dfb56ca96f3144b65fc12a81f4e1a6
Vlad Sergeychuk <vlad@lionhearth.com>
2022-01-02 16:06:40 +0800

    Урок 14. ДЗ. Задача 3. Создал классы и тела методов.

---
+++ src/ru/progwards/java1/lessons/queues/Calculate.java
@@ -0,0 +1,22 @@
+/*
+Class Calculate, который содержит методы:
+
+3.7 public static double calculation1(), возвращающую результат вычисления следующей формулы:
+2.2*(3+12.1), используя класс StackCalc
+
+3.8 public static double calculation2(), возвращающую результат вычисления следующей формулы:
+(737.22+24)/(55.6-12.1)+(19-3.33)*(87+2*(13.001-9.2)), используя класс StackCalc
+
+ */
+package ru.progwards.java1.lessons.queues;
+
+public class Calculate {
+
+    public static double calculation1(){
+        return 0.0;
+    }
+
+    public static double calculation2(){
+        return 0.0;
+    }
+}
--- src/ru/progwards/java1/lessons/queues/StackCalc.java
+++ src/ru/progwards/java1/lessons/queues/StackCalc.java
@@ -35,4 +35,19 @@ Class Calculate, который содержит методы:
 package ru.progwards.java1.lessons.queues;
 
 public class StackCalc {
+
+    public void push(double value){}
+
+    public double pop(){
+        return 0.0;
+    }
+
+    public void add(){}
+
+    public void sub(){}
+
+    public void mul(){}
+
+    public void div(){}
+
 }
